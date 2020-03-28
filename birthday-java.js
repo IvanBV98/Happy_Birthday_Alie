ccee48fd158cdf9ab7ee8bc226d7a60bd1c5fb98
IvanVB1998 <60651851+IvanVB1998@users.noreply.github.com>
2020-03-28 19:30:50 +0500

    New test

--- birthday-java.js
+++ birthday-java.js
@@ -1,16 +1,16 @@
 // console.log(window.innerWidth);
 // console.log(window.innerHeight);
 // Слайдер
-let button = document.querySelector('.past'), a = 0, e;
+let button = document.querySelector('.dbutton'),
+past = document.querySelector('.past'),
+a = 0,
+e;
 button = document.addEventListener('click', function() {
     otmena(e);
-    if (a == 0) {
-        a = 1;
-    }
-    else {
-        a = 0;
-    }
     return(a);
+});
+past = document.addEventListener('click', function() {
+    a = a + 1;
 });
 function otmena(e) {
     let evt = e ? e : window.event;
