2ac8ce1818465399a17815123204949764ad8709
IvanVB1998 <60651851+IvanVB1998@users.noreply.github.com>
2020-03-28 17:46:19 +0500

    New test

--- birthday-java.js
+++ birthday-java.js
@@ -1,2 +1,10 @@
 // console.log(window.innerWidth);
 // console.log(window.innerHeight);
+// Слайдер
+let button = document.querySelector('.dbutton'),
+a = 0;
+button = addEventListener('click', function() {
+    if (a==0) a=1;
+    else a=0;
+})
+console.log(a);
