010cabe422ee6fb01b3c07c51f074dccb77b259c
IvanVB1998 <60651851+IvanVB1998@users.noreply.github.com>
2020-04-07 03:07:46 +0500

    Готовый проект

--- birthday-java.js
+++ birthday-java.js
@@ -1,26 +1,47 @@
 // console.log(window.innerWidth);
 // console.log(window.innerHeight);
+
+// Запрос ключа доступа
+
+let security = document.querySelector('.security');
+login = prompt('Введите логин');
+while (login != 'Alie704') {
+    alert('Доступ запрещён');
+    security.style.display = 'block';
+    login = prompt('Введите логин');
+}
+alert('Доступ разрешён');
+security.style.display = 'none';
+
 // Слайдер
-let button = document.querySelector('.dbutton'),
-next = document.querySelector('.next'),
-a = 0,
-i = 1,
-k,
-e;
 
-button = document.addEventListener('click', function() {
+let past = document.querySelector('.past'),
+    next = document.querySelector('.next'),
+    button = document.querySelector('.dbutton'),
+    images = document.querySelectorAll('.image'),
+    active = document.querySelector('.active'),
+    fody = document.querySelector('.fody'),
+    i = 0,
+    e;
+
+
+button = document.addEventListener('click', function () {
     otmena(e);
-});
-next = document.addEventListener('click', function() {
-    if(i != 1) {
-        for (j = 1; j <= 0; j = j - 0.1) {
-
-        }
-    }
-});
+})
 function otmena(e) {
     let evt = e ? e : window.event;
     (evt.preventDefault) ? evt.preventDefault() : evt.returnValue = false;
     return false;
 }
-console.log(a);
+let before = past.addEventListener('click', function () {
+    images[i].classList.toggle("active");
+    if (i > 0) i = i - 1;
+    else i = 54;
+    images[i].classList.toggle("active");
+})
+let after = next.addEventListener('click', function () {
+    images[i].classList.toggle("active");
+    if (i < 54) i = i + 1;
+    else i = 0;
+    images[i].classList.toggle("active");
+})
--- test.js
+++ test.js
@@ -1,35 +1,5 @@
-let past = document.querySelector('.past'),
-    next = document.querySelector('.next'),
-    currentSlide = 0,
-    image = document.querySelector('.next'),
-    i = 0,
-    e;
-var imageClasses = image.classList;
-for (i = 0; i <= 54; i++) {
-    image[i] = document.querySelector('.image'+i);
-}
 
-button = document.addEventListener('click', function () {
-    otmena(e);
-})
-function otmena(e) {
-    let evt = e ? e : window.event;
-    (evt.preventDefault) ? evt.preventDefault() : evt.returnValue = false;
-    return false;
-}
-let before = past.addEventListener('click', function () {
-    pastSlide();
-})
-let after = next.addEventListener('click', function () {
-    if (i < 54) i = i + 1;
-    else i = 0;
-    console.log(i);
-})
-
-function pastSlide() {
-    console.log(image.classList)
-    image[i].classList.toggle("active");
-    if (i > 0) i = i - 1;
-    else i = 54;
-    image[i].classList.toggle("active");
-}
+fody.style.display = 'none';
+let x = prompt('Введите Ваш логин');
+let y = prompt('Введите Ваш пароль');
+if (x = 'Alie' && y == 704) fody.style.display != 'none';
