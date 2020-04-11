1aeac4006928c4268f8c77ea6699e8922dc76a69
IvanVB1998 <60651851+IvanVB1998@users.noreply.github.com>
2020-04-11 17:55:27 +0500

    OL

--- birthday-java.js
+++ birthday-java.js
@@ -5,7 +5,7 @@
 
 let security = document.querySelector('.security');
 login = prompt('Введите логин');
-while (login != 'Alie1234') {
+while (login != 'Alie704') {
     alert('Доступ запрещён');
     security.style.display = 'block';
     login = prompt('Введите логин');
